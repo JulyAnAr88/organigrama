ff00dd7b4f25a8191d00c943272318766d4e290c
Julián Aragón <85805374+JulyAnAr88@users.noreply.github.com>
2024-06-27 20:08:49 -0300

    Update conect

--- scripts/conexiones/conexionDatamartPentaho.js
+++ scripts/conexiones/conexionDatamartPentaho.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg")
 // Coloca aquí tus credenciales
 const pool = new Pool({
-  user: "dashboards",
-  host: "pg14olap.santafeciudad.gov.ar",
-  database: "datamart",
-  password: "L5-9he.2U348",
+  user: "",
+  host: "",
+  database: "",
+  password: "",
   port: 5432,
 });
 module.exports = pool;
